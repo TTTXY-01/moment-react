7d36cbcc2919d443bede73b5d3ca3b8c0e5abc67
dllo <42916302@qq.com>
2017-08-25 09:43:35 +0800

    123

--- src/components/transceiver/Recommendation_TING.js
+++ src/components/transceiver/Recommendation_TING.js
@@ -1,26 +1,23 @@
-import React, {Component} from 'react'
-import Recommendation from '../../components/transceiver/Recommendation_TING'
+import React, { Component } from 'react'
 const md5 = require('md5')
 const dateformat = require('dateformat')
 const base64 = require('Base64')
 
-class App extends Component {
+class Recommendation extends Component {
   constructor (props) {
     super(props)
     this.state = {
       data: []
     }
   }
-  // http://pianke.me/version5.0/pub/ad.php?type=3&sig=0E35C28AD161573F2E81ACF51B671ED8
-  componentDidMount () {
+  ajaxData = (interFace) => {
     const time = new Date()
-  // 2.根据当前时间, 进行格式化 yyyymmddHHMMss
+    // 2.根据当前时间, 进行格式化 yyyymmddHHMMss
     const timestamp = dateformat(time.getTime(), 'yyyymmddHHMMss')
-  // 3.将字符串 0+''+timestamp 转成MD5, 并变为全大写2:5: 'fetch' is not defined.
-
+    // 3.将字符串 0+''+timestamp 转成MD5, 并变为全大写
     const sig = md5('0' + '' + timestamp).toUpperCase()
     const Authorization = base64.btoa('' + ':' + timestamp)
-    const url = '/api/pub/ad.php?type=3&sig=' + sig
+    const url = '/api' + interFace + '&sig=' + sig
     fetch(url, {
       method: 'GET',
       headers: {
@@ -31,19 +28,30 @@ class App extends Component {
         return response.json()
       })
       .then(response => {
+        console.log(response)
         this.setState({
           data: response.data
         })
       })
   }
-
-  render () {
+  componentDidMount () {
+    this.ajaxData('/ting/listOfRecommend.php?pageSize=3')
+  }
+  render() {
     return (
       <div>
-        <Recommendation />
+        {
+          this.state.data.map(function (item, index) {
+            return (
+              <div>
+                <p key={index.toString()}>{item.title}</p>
+              </div>
+          )
+          })
+        }
       </div>
     )
   }
 }
 
-export default App
+export default Recommendation
