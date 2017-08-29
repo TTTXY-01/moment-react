5594bfd684c9829c94d840bfd1524f678922b91b
dllo <dllo@liang.local>
2017-08-29 08:36:28 +0800

    碎片页瀑布流

--- src/modules/fragment/index.js
+++ src/modules/fragment/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './App' // 引入组件App
 import '../../assets/styles/fragment.styl'  // 引入css
 import '../../assets/styles/read.styl'
+import '../../assets/styles/Footer.styl'
 ReactDOM.render(
   <App />,
   document.getElementById('app')
