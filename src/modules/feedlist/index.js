440e61cd411a150d324a3a8a8123cb43314ea744
dllo <dllo@liang.local>
2017-09-02 17:39:30 +0800

    动态页面

---
+++ src/modules/feedlist/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dllo on 17/9/1.
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import '../../assets/styles/feedlist.styl'
+import '../../assets/styles/read.styl'
+import '../../assets/styles/Footer.styl'
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
